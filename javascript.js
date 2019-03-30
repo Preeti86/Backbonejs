c28b7244f204cdd8a235a8606da375f05b96ad9e
Preeti <35522305+Preeti86@users.noreply.github.com>
2019-03-30 11:05:18 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function () {
-    var FirstView = Backbone.View.extend({
-        initialize:function () {
-            this.render();
-
-        },
-        render:function () {
-            console.log("This is a fuck off message");
-        }
-    });
-    var fview = new FirstView();
-
-});*/
 (function () {
 
     var Rectangle = Backbone.Model.extend({});
